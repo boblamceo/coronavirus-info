c46717222e97b7e072a0e72d71621f425615fe9c
Bob Lam <boblam.ceo@gmail.com>
2020-05-26 20:56:12 +0800

    fix react-helmet problem

--- pages/howTo.tsx
+++ pages/howTo.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Helmet from 'react-helmet';
+import {Helmet} from 'react-helmet';
 import ReactPlayer from 'react-player';
 import { IoIosWater } from 'react-icons/io';
 
